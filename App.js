a465f3041bce0fab5c3f2f987d958b081db261d3
nottak0409 <nottak0409@gmail.com>
2021-06-28 20:05:52 +0900

    container style change

--- App.js
+++ App.js
@@ -13,14 +13,26 @@ const styles = StyleSheet.create({
       height: 100,
       width: '100%',
       borderColor: "gray",
-      borderWidth: 1
+      borderWidth: 1,
+      flexDirection: "row"
+  },
+  leftContainer: {
+      backgroundColor: "red",
+      width:100,
+  },
+  rightContainer: {
+      backgroundColor: "blue",
+      flex: 1
   }
 });
 
 export default function App() {
   return (
     <View style={styles.container}>
-        <View style={styles.itemContainer}></View>
+        <View style={styles.itemContainer}>
+            <View style={styles.leftContainer}></View>
+            <View style={styles.rightContainer}></View>
+        </View>
     </View>
   );
 }
