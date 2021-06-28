68d0a1ac9b2aadb1365de8581ac8b54f36461ee1
nottak0409 <nottak0409@gmail.com>
2021-06-28 20:02:46 +0900

    style class

--- App.js
+++ App.js
@@ -2,20 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-        <View
-            style={{
-                height: 100,
-                width: '100%',
-                borderColor: "gray",
-                borderWidth: 1
-            }}></View>
-    </View>
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -23,4 +9,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  itemContainer: {
+      height: 100,
+      width: '100%',
+      borderColor: "gray",
+      borderWidth: 1
+  }
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+        <View style={styles.itemContainer}></View>
+    </View>
+  );
+}
