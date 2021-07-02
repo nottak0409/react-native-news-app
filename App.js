b189a73a387f5e7dbe325f0244d01b231d0944cf
nottak0409 <nottak0409@gmail.com>
2021-07-03 06:49:57 +0900

    修正

--- App.js
+++ App.js
@@ -1,70 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, FlatList, SafeAreaView } from 'react-native';
-import ListItem from './components/ListItem.js';
-import dummyarticles from './dummies/articles.json';
-import Constants from 'expo-constants';
-import axios from 'axios';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./screens/HomeScreen";
 
-const URL = `https://newsapi.org/v2/top-headlines?country=jp&apiKey=${Constants.manifest.extra.newsApiKey}`;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  itemContainer: {
-      height: 100,
-      width: '100%',
-      borderColor: "gray",
-      borderWidth: 1,
-      flexDirection: "row"
-  },
-  leftContainer: {
-      width:100,
-  },
-  rightContainer: {
-      flex: 1,
-      flexDirection: 'column',
-      padding: 10,
-      justifyContent: "space-between"
-  },
-  text: {
-      fontSize: 16
-  },
-  subtext: {
-      fontSize: 12,
-      color: "gray"
-  }
-});
+const Stack = createStackNavigator();
 
 export default function App() {
-    const [articles, setArticles] = useState([]);
-    useEffect(() => {
-        fetchArtiCles()
-    }, []);
-
-    const fetchArtiCles = async() => {
-        try{
-            const response = await axios.get(URL);
-            setArticles(response.data.articles);
-        } catch(error) {
-            console.log(error);
-        }
-    }
   return (
-    <SafeAreaView style={styles.container}>
-        <FlatList
-            data={articles}
-            renderItem={({item}) => (
-                <ListItem
-                    imageUrl = {item.urlToImage}
-                    title = {item.title}
-                    author = {item.author}
-                    />
-            )}
-            keyExtractor={(item, index) => index.toString()}
-        />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
