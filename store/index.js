1275af79faae03feaec71d86e87dbcf8bf9fcd57
nottak0409 <nottak0409@gmail.com>
2021-07-04 08:28:53 +0900

    debuggerコメントアウト

--- store/index.js
+++ store/index.js
@@ -1,11 +1,11 @@
 import {createStore, combineReducers} from 'redux';
 import userReducer from './reducers/user';
-import {composeWithDevTools} from 'redux-devtools-extension';
+//import { composeWithDevTools } from 'redux-devtools-extension';
 
 const rootReducer = combineReducers ({
     user: userReducer
 })
 
-const store = createStore(rootReducer, composeWithDevTools);
+const store = createStore(rootReducer);
 
 export default store;
