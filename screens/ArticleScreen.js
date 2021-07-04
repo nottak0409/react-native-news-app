5af4ce3d89a14209297210f98b128cf2b7eb3fa5
nottak0409 <nottak0409@gmail.com>
2021-07-04 09:06:30 +0900

    add dispatch

--- screens/ArticleScreen.js
+++ screens/ArticleScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { StyleSheet, SafeAreaView, Text} from 'react-native';
+import { StyleSheet, SafeAreaView, Text, TouchableOpacity} from 'react-native';
 import { WebView } from 'react-native-webview';
+import { useDispatch } from 'react-redux';
+import { addClip, deleteClip } from '../store/actions/user';
 
 const styles = StyleSheet.create({
     container: {
@@ -11,8 +13,16 @@ const styles = StyleSheet.create({
 
 function ArticleScreen({route}) {
     const {article} = route.params;
+
+    const dispatch = useDispatch()
     return (
         <SafeAreaView style={styles.container}>
+            <TouchableOpacity onPress={() => { dispatch(addClip({clip: article}))} }>
+                <Text style={{ margin:10, fontSize: 30}}>add Clip</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => { dispatch(deleteClip({clip: article}))} }>
+                <Text style={{ margin:10, fontSize: 30}}>DELETE Clip</Text>
+            </TouchableOpacity>
             <WebView source={{ uri: article.url}}></WebView>
         </SafeAreaView>
     );
