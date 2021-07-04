c4ddbdf7757c42d70bdb8fb71a54279b3914a54d
nottak0409 <nottak0409@gmail.com>
2021-07-04 12:08:55 +0900

    reduxの永続化機能追加

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import {Provider} from "react-redux";
 import AppNavigator from "./navigation/AppNavigator";
-import store from "./store"
+import store, { persistor } from "./store"
+import { PersistGate } from 'redux-persist/integration/react';
 
 export default function App() {
   return (
-      <Provider store={store}>
-          <AppNavigator>
-        </AppNavigator>
-      </Provider>
-  );
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <AppNavigator>
+                </AppNavigator>
+            </PersistGate>
+        </Provider>
+    );
 }
--- store/index.js
+++ store/index.js
@@ -1,11 +1,22 @@
 import {createStore, combineReducers} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import {AsyncStorage} from 'react-native';
+import {persistReducer, persistStore} from 'redux-persist';
 import userReducer from './reducers/user';
-//import { composeWithDevTools } from 'redux-devtools-extension';
 
-const rootReducer = combineReducers ({
-    user: userReducer
-})
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['user'],
+};
 
-const store = createStore(rootReducer);
+const rootReducer = combineReducers({
+  user: userReducer,
+});
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(persistedReducer, composeWithDevTools());
+
+export const persistor = persistStore(store);
 export default store;
